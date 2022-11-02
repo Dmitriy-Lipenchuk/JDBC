3cc03f6464bae6c425e6c856a6fe5cdc699c511b
Dmitriy Lipenchuk <dmitriy.lipenchuk.01@inbox.ru>
2022-11-02 23:17:39 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("java")
+}
+
+group = "ru.gamesphere"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+allprojects {
+    apply(plugin = "java")
+
+    dependencies {
+        implementation("com.intellij:annotations:12.0")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "JDBC"
+include("accounting-system")
